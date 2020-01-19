2fca2a1a10f5a9d3df162ea32904dedda09df719
jaredkgrove <jaredkgrove@gmail.com>
2020-01-18 18:15:21 -0600

    added technology logos

--- src/AboutPane.jsx
+++ src/AboutPane.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components'
+import logo from './logo.svg'
 
 function AboutPane() {
     return (    
@@ -9,13 +10,35 @@ function AboutPane() {
                 As a Mechanical Engineer turned Software Engineer, I am passionate about creating reliable, intuitive tools for both users and developers. Full-stack developer drawn to the creativity of the front-end and the logic of the back-end. Experienced working with interdisciplinary teams producing quality work in a positive environment.
             </ElevatorPitch>
             <TechnologiesWrapper>
-                <TechnologyCard>JavaScript</TechnologyCard>
-                <TechnologyCard>Ruby</TechnologyCard>
-                <TechnologyCard>Rails</TechnologyCard>
-                <TechnologyCard>React</TechnologyCard>
-                <TechnologyCard>Redux</TechnologyCard>
-                <TechnologyCard>RSpec</TechnologyCard>
-                <TechnologyCard>Rails</TechnologyCard>
+                <TechnologyCard >
+                    <a title="Chris Williams [Public domain]" href="https://commons.wikimedia.org/wiki/File:Unofficial_JavaScript_logo_2.svg">
+                        <img  alt="Unofficial JavaScript logo 2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png"/>
+                    </a>
+                </TechnologyCard>
+
+                    
+                <TechnologyCard >
+                    <a href="\#">
+                        <img src={logo} alt="React Logo"/>
+                    </a>
+                </TechnologyCard>
+
+                <TechnologyCard>
+                    <a title="Yukihiro Matsumoto, Ruby Visual Identity Team [CC BY-SA (https://creativecommons.org/licenses/by-sa/2.5)]" href="https://commons.wikimedia.org/wiki/File:Ruby_logo.svg">
+                        <img alt="Ruby logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/128px-Ruby_logo.svg.png"/>
+                    </a>
+                </TechnologyCard>
+                <TechnologyCard>
+                    <a title="Jamie Dihiansan http://weblog.rubyonrails.org/2016/1/19/new-rails-identity/2 [CC0]" href="https://commons.wikimedia.org/wiki/File:Ruby_On_Rails_Logo.svg">
+                        <img  alt="Ruby On Rails Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Ruby_On_Rails_Logo.svg/256px-Ruby_On_Rails_Logo.svg.png"/>
+
+                    </a>
+                </TechnologyCard>
+                <TechnologyCard>
+                    <a title="AAMINE1965 [CC BY-SA (https://creativecommons.org/licenses/by-sa/4.0)]" href="https://commons.wikimedia.org/wiki/File:Redux.png">
+                        <img alt="Redux" src="https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png"/>
+                    </a>
+                </TechnologyCard>
             </TechnologiesWrapper>
         </AboutPaneWrapper>
     );
@@ -43,16 +66,26 @@ const TechnologiesWrapper = styled.div`
     float:right;
     text-align: left;
     background: grey;
-    display: flex;
-    justify-content: space-around;
-    align-content: space-around;
-    flex-wrap: wrap;
-`;
 
+`;
+// display: flex;
+// justify-content: space-around;
+// align-content: space-around;
+
+// flex-wrap: wrap;
 const TechnologyCard = styled.div`
+    float: left;
     text-align: center;
-    height: 150px;
-    width: 100px;
-    background: white;
+    height: 100px;
+    min-width: 100px;
     margin: 20px;
+    overflow: hidden;
+    > *{
+
+        > *{
+            object-fit: contain;
+            max-width: 100%; 
+            height: 100%;
+        }
+    }
 `;
--- src/IntroPane.jsx
+++ src/IntroPane.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 import styled from 'styled-components'
 
 function IntroPane() {
+    const can = useRef(null)
+    let ctx = null
+    let canvasLineArray = []
+
+    let canvasWidth = null
+    let canvasHeight = null
+    const drawBackground = () => {
+        if(can.current){
+            ctx = can.current.getContext('2d')
+            canvasWidth = can.current.clientWidth
+            canvasHeight = can.current.clientHeight
+            can.current.width = canvasWidth
+            can.current.height = canvasHeight
+            let currentY = 0
+            let lineWidth = 10
+            let color = 'hsl(60,60%,60%)'
+            do {
+                if(currentY % 20 === 0){
+                    canvasLineArray.push(new CanvasLine(0 - Math.random()*5000, currentY, lineWidth, color))
+                }else{
+                    canvasLineArray.push(new CanvasLine(canvasWidth + Math.random()*5000, currentY, lineWidth, color))
+                }
+                currentY += lineWidth
+            } while (currentY < canvasHeight);
+            // for (let i = 0; i < 100; i++) {
+            //     canvasLineArray.push(new CanvasLine(Math.random() * canvasWidth, Math.random() * canvasHeight))
+            // }
+            // canvasLineArray.push(new CanvasLine(0 - Math.random()*1000, Math.random() * canvasHeight, lineWidth, color))
+            animate()
+        }
+    }
+
+
+    const animate = () => {
+        requestAnimationFrame(animate)
+        ctx.clearRect(0,0, canvasWidth, canvasHeight);
+        canvasLineArray.forEach((canvasLine) => canvasLine.draw(ctx, canvasWidth, canvasHeight))
+    }
     return (    
-        <IntroPaneWrapper>
+        <IntroPaneWrapper onClick={drawBackground}>
+            <CoolCanvas ref={can} />
             <h1>Jared Grove</h1>
             <h3>Full Stack Engineer</h3>
         </IntroPaneWrapper>
@@ -17,5 +56,46 @@ const IntroPaneWrapper = styled.div`
   height: 100vh;
   width: 100vw;
   text-align: center;
+  color:grey;
 `;
 
+const CoolCanvas = styled.canvas`
+    z-index: -1;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 100vh;
+    width: 100vw;
+    background:grey;
+`;
+
+
+function CanvasLine(x, y, lineWidth, color){
+    this.x1 = x;
+    this.y1 = y;
+    this.lineWidth = lineWidth;
+    this.length = 0
+    this.color = color
+};
+   
+CanvasLine.prototype.draw = function(ctx, maxX, maxY) {
+    ctx.beginPath();
+    ctx.moveTo(this.x1, this.y1);
+
+    if(this.x1 <= 0){
+        ctx.lineTo(this.x1 + this.length, this.y1);
+    }else{
+        ctx.lineTo(this.x1 - this.length, this.y1);
+    }
+
+    ctx.lineWidth = this.lineWidth
+    ctx.strokeStyle = this.color
+    ctx.stroke();
+
+    if(this.length - Math.abs(this.x1) > maxX || this.y1 > maxY){
+        
+    }else{
+        this.length += 50
+    }
+};
+   
